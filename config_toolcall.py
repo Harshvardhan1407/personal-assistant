b9ca98f3a2c46c4aa605c73ebe047cc614f99f8a
Harshvardhan1407 <sharmaharshvardhan1407@gmail.com>
2024-08-06 18:10:55 +0530

    tool call function PBR

---
+++ config_toolcall.py
@@ -0,0 +1,89 @@
+tools = [
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "consumer_details",
+                        "description": "Fetches consumer details, grid_reading, balance based on the request.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+# 1
+                 {
+                    "type": "function",
+                    "function": {
+                        "name": "notifiation",
+                        "description": "what notification are enabled for me",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+# 2
+                 {
+                    "type": "function",
+                    "function": {
+                        "name": "balance_recharge",
+                        "description": "give me my balance or recharge related query",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+# 3
+                 {
+                    "type": "function",
+                    "function": {
+                        "name": "consumption",
+                        "description": "Retrieve detailed consumption information including current grid reading, generator and grid readings, daily and monthly units, amounts, fixed charges, and last updated timestamps.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+# 4
+                 {
+                    "type": "function",
+                    "function": {
+                        "name": "site_details",
+                        "description": "what are my site details",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+
+                 {
+                    "type": "function",
+                    "function": {
+                        "name": "costumer_support",
+                        "description": "give me my customer support details",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+                 {
+                    "type": "function",
+                    "function": {
+                        "name": "power_cut",
+                        "description": "power cut or electricity outage",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "daily_data",
+                        "description": "give me this day or  daily data",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "monthly_data",
+                        "description": "give me this month data or monthly data",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {},  # No properties needed as it takes no parameters
+                            },},},
+            ]
