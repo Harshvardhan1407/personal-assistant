5636e3327c508d97172be177851cf68a72ad6fa9
Harshvardhan1407 <sharmaharshvardhan1407@gmail.com>
2024-08-06 18:12:44 +0530

    new chat per user

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
-from main import OpenAIBot
+from main import OpenAIBot, logger
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
-
 app = Flask(__name__)
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "supersecretkey")
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # List of users
-users = {'amit': 'amit','akhilesh': 'akhilesh','akshay': 'akshay','anshu': 'anshu','ashutosh': 'ashutosh','dolly': 'dolly','harsh': 'harsh','hridendra': 'hridendra','pallav': 'pallav','pranav': 'pranav','rahul': 'rahul','shubham': 'shubham','shubhangi': 'shubhangi','surbhi': 'surbhi','yash': 'yash','zaira': 'zaira'}
-
-# session_id = str(Session(app))
+users = {
+    'amit': 'amit', 'akhilesh': 'akhilesh', 'akshay': 'akshay', 'anshu': 'anshu',
+    'ashutosh': 'ashutosh', 'dolly': 'dolly', 'harsh': 'harsh', 'hridendra': 'hridendra',
+    'pallav': 'pallav', 'pranav': 'pranav', 'rahul': 'rahul', 'shubham': 'shubham',
+    'shubhangi': 'shubhangi', 'surbhi': 'surbhi', 'yash': 'yash', 'zaira': 'zaira'
+}
 chatbot = OpenAIBot(os.getenv("GPT_MODEL"))
-# Session(app)
 
 def get_conversation_filename():
     return f'conversation_{datetime.now().strftime("%Y_%m_%d")}.json'
@@ -24,37 +25,33 @@ def load_conversation(filename):
     try:
         if os.path.exists(filename):
             with open(filename, 'r') as file:
+                # logger.info(f"{filename} file found and loaded")
                 return json.load(file)
-                # if username in data.keys():
-                #     print("here1")
-                #     return data[username] #data[username]
-                # print("here2")
-                # return {} #json.load(file)
+            
         else:
-            with open(filename,"w") as file:
+            with open(filename, "w") as file:
+                logger.info(f"{filename} file not found, creating a new file")
                 json.dump({}, file)
-        return {}
+            return {}                
     except Exception as e:
-        print("error in load_conversation:",e)
-
+        logger.info("error in load_conversation:", e)
+        return {}
+    
 def save_conversation(filename, conversation):
     try:
         with open(filename, 'w') as file:
             json.dump(conversation, file, indent=4)
     except Exception as e:
-        print("error in save_conversation:",e)
-
+        logger.info("error in save_conversation:", e)
 
 @app.route('/')
 def index():
     try:
-        print(session['username'])
         if 'username' not in session:
             return redirect(url_for('login'))
         return render_template('index.html')
     except Exception as e:
-        print("error in index:",e)
-
+        logger.info("error in index:", e)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -64,19 +61,16 @@ def login():
             password = request.form['password']
             if username in users and users[username] == password:
                 session['username'] = username
-                # print(session['username'])
                 filename = get_conversation_filename()
                 main_conversation = load_conversation(filename)
-                if username in main_conversation.keys():
-                    chatbot.conversation = main_conversation[username]
-                # else:
-                #     chatbot.conversation = []
+                if username in main_conversation:
+                    chatbot.conversations[username] = main_conversation[username]
                 return redirect(url_for('index'))
             else:
                 return 'Invalid username or password', 401
         return render_template('login.html')
     except Exception as e:
-        print("error in login:",e)
+        logger.info("error in login:", e)
         return "An error occurred", 500
 
 @app.route('/logout')
@@ -85,59 +79,34 @@ def logout():
         session.pop('username', None)
         return redirect(url_for('login'))
     except Exception as e:
-        print("error in logout:",e)
+        logger.info("error in logout:", e)
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    
-    """
-    session_id = request.cookies.get('s_k')  # Get session key from cookies
-    if not session_id:
-        return 'No session key found', 400
-    """
-    username = session['username']
-    print(session['username'])
-    # Get Prompt from User  
-    prompt = request.form['prompt']
-    # User can stop the chat by sending 'End Chat' as a Prompt
-    if prompt.upper() == 'END CHAT':
-        return 'END CHAT'
-    response = chatbot.generate_response(prompt)
+    try:
+        if 'username' not in session:
+            return redirect(url_for('login'))
+            
+        username = session['username']
+        prompt = request.form['prompt']
 
-    filename = get_conversation_filename()
-    conversation_data = load_conversation(filename)
-    # print(conversation_data)
-    # print(conversation_data.keys())
-    # print("conversation:", conversation_data)
-    # print("type",type(conversation_data))
-    # if type(conversation_data) == dict:
-    if username not in conversation_data.keys():
-        conversation_data[username] = []
-        conversation_data[username].append({"prompt": prompt, "response": response})
-    else:
-        conversation_data[username].append({"prompt": prompt, "response": response})
-    # Save the updated conversation data
-    save_conversation(filename, conversation_data)
-    return response
-    """
-    # Generate and Print the Response from ChatBot
-    response = chatbot.generate_response(prompt)
-    # conversation_file()
-    # chatbot.save_conversation(get_conversation_filename(),session_id)  # Save conversation after generating response
-    # Update the chatbot conversation
-    conversation_data = conversation_file()
-    
-    session_id = session['username']
-    if session_id not in conversation_data.keys():
-        conversation_data[session_id] = []
-    conversation_data[session_id].append({"prompt": prompt, "response": response})
-    save_chat(conversation_data)
+        # User can stop the chat by sending 'End Chat' as a Prompt
+        if prompt.upper() == 'END CHAT':
+            return 'END CHAT'
+        
+        response = chatbot.generate_response(username, prompt)
 
-    return response
-    """
+        filename = get_conversation_filename()
+        conversation_data = load_conversation(filename)
+        if username not in conversation_data:
+            conversation_data[username] = []
+        conversation_data[username].append({"prompt": prompt, "response": response})
+        
+        # Save the updated conversation data
+        save_conversation(filename, conversation_data)
+        return response
+    except Exception as e:
+        logger.info("error in chat:",e)
+# 14:31
 if __name__ == '__main__':
-    # filename = get_conversation_filename()
-    # load_conversation(filename)  # Load the previous conversation if the file exists
-    app.run(debug=True, host='0.0.0.0', port=5011)
+    app.run(debug=True, host='0.0.0.0', port=5001)
