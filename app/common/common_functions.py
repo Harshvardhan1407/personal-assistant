0f2661c4c23ce26eebf1c0fa1eccaa4626da50f8
Harshvardhan714 <harshvardhansharma1407@gmail.com>
2025-03-19 14:21:27 +0530

    user connection

--- app/common/common_functions.py
+++ app/common/common_functions.py
@@ -1,6 +1,5 @@
 import re
 from IPython.display import display, Audio
-import soundfile as sf
 import winsound
 import uuid
 import hashlib
@@ -21,22 +20,22 @@ def clean_response(response):
     """Removes <think>...</think> tags and their content from the response."""
     return re.sub(r"<think>.*?</think>", "", response, flags=re.DOTALL).strip()
 
-def run_tts(pipeline, text):
-    generator = pipeline(
-        text, voice='af_bella',
-        speed=1, split_pattern=r'\n+'
-    )
-    for i, (gs, ps, audio) in enumerate(generator):
-        # print(i)  # i => index
-        # print(gs) # gs => graphemes/text
-        # print(ps) # ps => phonemes
-        display(Audio(data=audio, rate=24000, autoplay=i==0))
-        sf.write(f'audio_samples/{i}.wav', audio, 24000) # save each audio file
+# def run_tts(pipeline, text):
+#     generator = pipeline(
+#         text, voice='af_bella',
+#         speed=1, split_pattern=r'\n+'
+#     )
+#     for i, (gs, ps, audio) in enumerate(generator):
+#         # print(i)  # i => index
+#         # print(gs) # gs => graphemes/text
+#         # print(ps) # ps => phonemes
+#         display(Audio(data=audio, rate=24000, autoplay=i==0))
+#         sf.write(f'audio_samples/{i}.wav', audio, 24000) # save each audio file
 
-def play_audio():
-    audio_path = "audio_samples/0.wav"
-    # Play the sound (SND_FILENAME makes sure it is treated as a file)
-    winsound.PlaySound(audio_path, winsound.SND_FILENAME)
+# def play_audio():
+#     audio_path = "audio_samples/0.wav"
+#     # Play the sound (SND_FILENAME makes sure it is treated as a file)
+#     winsound.PlaySound(audio_path, winsound.SND_FILENAME)
 
 def generate_uuid_from_string(identifier):
     """Generate a deterministic UUID from a given string (e.g., email)."""
